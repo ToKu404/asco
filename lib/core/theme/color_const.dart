cdab3500fffb8bd47111a50a877279bbef7f8a95
toku404 <phantom26isn@gmail.com>
2023-02-01 12:25:29 +0800

    new ini

--- lib/core/theme/color_const.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-Color kBlack = const Color(0xFF222831);
-Color kBlackVariant1 = const Color(0xFF1D272C);
-Color kBlackVariant2 = const Color(0xFF6A7879);
-Color kToal = const Color(0xFF00ADB5);
-Color kToalVariant1 = const Color(0xFF109BA1);
-Color kWhite = const Color(0xFFEEEEEE);
-Color kGrey = const Color(0xFF99ADAE);
