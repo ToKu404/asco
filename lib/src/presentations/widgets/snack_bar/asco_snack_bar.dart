4a1b3a8e4dee34359178e927ab9c2279d0ea234e
fajri-rasid1st <fajrimuhammad460@gmail.com>
2023-02-24 13:56:59 +0800

    fix: Snackbar Animation [FIXED :white_check_mark: ]

--- lib/src/presentations/widgets/snack_bar/asco_snack_bar.dart
+++ lib/src/presentations/widgets/snack_bar/asco_snack_bar.dart
@@ -29,102 +29,98 @@ class AscoSnackBar extends StatelessWidget {
     final hsl = HSLColor.fromColor(color ?? contentType.color!);
     final hslDark = hsl.withLightness((hsl.lightness - 0.1).clamp(0.0, 1.0));
 
-    return SafeArea(
-      maintainBottomViewPadding: true,
-      child: Stack(
-        clipBehavior: Clip.none,
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.symmetric(
-              horizontal: width * 0.05,
-              vertical: height * 0.025,
-            ),
-            decoration: BoxDecoration(
-              color: color ?? contentType.color,
-              borderRadius: BorderRadius.circular(16),
-            ),
-            child: Row(
-              children: <Widget>[
-                const Spacer(),
-                Expanded(
-                  flex: 8,
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Expanded(
-                            flex: 3,
-                            child: Text(
-                              title,
-                              style: kTextTheme.titleMedium?.copyWith(
-                                fontWeight: FontWeight.w600,
-                                color: Palette.white,
-                              ),
+    return Stack(
+      clipBehavior: Clip.none,
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.symmetric(
+            horizontal: width * 0.05,
+            vertical: height * 0.025,
+          ),
+          decoration: BoxDecoration(
+            color: color ?? contentType.color,
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Row(
+            children: <Widget>[
+              const Spacer(),
+              Expanded(
+                flex: 8,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Flexible(
+                          child: Text(
+                            title,
+                            style: kTextTheme.titleMedium?.copyWith(
+                              fontWeight: FontWeight.w600,
+                              color: Palette.white,
                             ),
                           ),
-                          InkWell(
-                            onTap: () => ScaffoldMessenger.of(context)
-                                .hideCurrentSnackBar(),
-                            child: SvgPicture.asset(
-                              AssetPath.getIcons('failure_outlined.svg'),
-                              height: height * 0.022,
-                            ),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(height: 4),
-                      Text(
-                        message,
-                        style: kTextTheme.bodyMedium?.copyWith(
-                          color: Palette.white,
                         ),
+                        InkWell(
+                          onTap: () => ScaffoldMessenger.of(context)
+                              .hideCurrentSnackBar(),
+                          child: SvgPicture.asset(
+                            AssetPath.getIcons('failure_outlined.svg'),
+                            height: height * 0.022,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 4),
+                    Text(
+                      message,
+                      style: kTextTheme.bodyMedium?.copyWith(
+                        color: Palette.white,
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-              ],
+              ),
+            ],
+          ),
+        ),
+        Positioned(
+          bottom: 0,
+          child: ClipRRect(
+            borderRadius: const BorderRadius.only(
+              bottomLeft: Radius.circular(16),
+            ),
+            child: SvgPicture.asset(
+              AssetPath.getVector('bubbles.svg'),
+              height: height * 0.06,
+              width: width * 0.05,
+              color: hslDark.toColor(),
             ),
           ),
-          Positioned(
-            bottom: 0,
-            child: ClipRRect(
-              borderRadius: const BorderRadius.only(
-                bottomLeft: Radius.circular(16),
-              ),
-              child: SvgPicture.asset(
-                AssetPath.getVector('bubbles.svg'),
+        ),
+        Positioned(
+          top: -height * 0.02,
+          left: width * 0.02,
+          child: Stack(
+            alignment: Alignment.center,
+            children: <Widget>[
+              SvgPicture.asset(
+                AssetPath.getVector('message_bubble.svg'),
                 height: height * 0.06,
-                width: width * 0.05,
                 color: hslDark.toColor(),
               ),
-            ),
-          ),
-          Positioned(
-            top: -height * 0.02,
-            left: width * 0.02,
-            child: Stack(
-              alignment: Alignment.center,
-              children: <Widget>[
-                SvgPicture.asset(
-                  AssetPath.getVector('message_bubble.svg'),
-                  height: height * 0.06,
-                  color: hslDark.toColor(),
+              Positioned(
+                top: height * 0.015,
+                child: SvgPicture.asset(
+                  assetSvg(contentType),
+                  height: height * 0.022,
                 ),
-                Positioned(
-                  top: height * 0.015,
-                  child: SvgPicture.asset(
-                    assetSvg(contentType),
-                    height: height * 0.022,
-                  ),
-                )
-              ],
-            ),
+              )
+            ],
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 
